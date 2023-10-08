359c5cb049ce738bf9b3b3831b2f223e3e80de91
LukeHolmessba23038 <146835310+LukeHolmessba23038@users.noreply.github.com>
2023-10-08 18:59:34 +0100

    Add files via upload
    Second Upload, just showing progress

--- Luke Holmes CA01 Data Preparation Project.py
+++ Luke Holmes CA01 Data Preparation Project.py
@@ -1,28 +1,60 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[11]:
 
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-# In[2]:
+# In[ ]:
 
 
-file_path = 
 
 
-# In[6]:
+
+# In[20]:
 
 
+#Uploading the dataset and giving ity a shortcut name 'AFS'
 data = pd.read_csv('aps_failure_set.csv')
+afs=pd.read_csv('aps_failure_set.csv')
 
 
-# In[7]:
+# In[21]:
 
 
-print(data.head)
+# Display the first 5 records
+afs.head()
+
+
+# In[29]:
+
+
+#Display the first 10 rows of the dataset
+data.head(10)
+
+
+# In[28]:
+
+
+#Requesting basic info on the dataset
+data.info()
+
+
+# In[27]:
+
+
+#Basic Statistical Information on the dataset
+data.describe()
+
+
+# In[25]:
+
+
+afs.isnull().sum()
 
 
 # In[ ]:
