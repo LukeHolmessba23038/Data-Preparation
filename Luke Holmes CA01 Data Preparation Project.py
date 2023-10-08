c6f433969b01aa8d3a8af21c5b070e5541978f04
LukeHolmessba23038 <146835310+LukeHolmessba23038@users.noreply.github.com>
2023-10-08 18:43:38 +0100

    Add files via upload
    Initial upload and exploratory analysis of the file

---
+++ Luke Holmes CA01 Data Preparation Project.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+file_path = 
+
+
+# In[6]:
+
+
+data = pd.read_csv('aps_failure_set.csv')
+
+
+# In[7]:
+
+
+print(data.head)
+
+
+# In[ ]:
+
+
+
+
